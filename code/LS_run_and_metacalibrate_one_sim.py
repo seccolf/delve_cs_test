8b27053930707b60990787678c4b682fe61d571e
Lucas Secco <secco@uchicago.edu>
2021-10-14 13:36:11 -0500

    small changes to parallel running

--- code/LS_run_and_metacalibrate_one_sim.py
+++ code/LS_run_and_metacalibrate_one_sim.py
@@ -16,33 +16,38 @@ import time
 ################################################
 
 # SETUP ACTUAL RUN!
-SEED = 50#int(os.environ['SLURM_PROCID']) +50
-#PROCID = str(int(os.environ['SLURM_PROCID'])+1)
-meds_dir = '/home/secco/scratch-midway2/delve/simulated_meds/'
-input_medsname = 'TESTBED_DES0954+0252_r4918p01_i_meds-DECADE_copy5.fits' #missing a '.fits' for now
+NTASKS = int(os.environ['SLURM_NTASKS'])
+#start looping over possible seed numbers based on how many tasks are running:
 
-#now replace all of the objects in this by simulated objects
-time1 = time.time()
-replace_all_cutouts_by_sims(meds_dir+input_medsname, SEED, +0.02, 0.00, verbose=True)
-time2 = time.time()
-print('replace_cutout_by_sims took %1.2f minutes'%((time2-time1)/60.0))
+for loop_counter in np.arange(100): #each process will likely not have time to do all of these, but it's ok
+    SEED = int(os.environ['SLURM_PROCID']) + 50 + loop_counter*NTASKS
+    PROCID = str(int(os.environ['SLURM_PROCID'])+1)
+    print('PROCESS %s is starting calculation on SEED %d'%(PROCID,SEED))
+    meds_dir = '/home/secco/scratch-midway2/delve/simulated_meds/'
+    input_medsname = 'TESTBED_DES0954+0252_r4918p01_i_meds-DECADE_copy'+PROCID+'.fits'
 
-#now metacalibrate the positive g1 output:
-output_dir = '/home/secco/scratch-midway2/delve/simulated_mcal/'
-time3 = time.time()
-output = run_metacal([meds_dir+input_medsname],SEED)
-time4 = time.time()
-fitsio.write(output_dir+'metacal_output_seed'+str(SEED)+'_positive.fits', output, clobber=True)
-print('positive metacal run took %1.2f minutes'%((time4-time3)/60.0))
+    #now replace all of the objects in this by simulated objects
+    time1 = time.time()
+    replace_all_cutouts_by_sims(meds_dir+input_medsname, SEED, +0.02, 0.00, verbose=True)
+    time2 = time.time()
+    print('replace_cutout_by_sims took %1.2f minutes'%((time2-time1)/60.0))
 
-'''
-#now replace all of the objects in this by simulated objects
-replace_all_cutouts_by_sims(meds_dir+input_medsname,SEED, -0.02, 0.00, verbose=True)
-#now metacalibrate the negative g1 output:
-output_dir = '/home/secco/scratch-midway2/delve/simulated_mcal/'
-output = run_metacal([meds_dir+input_medsname],SEED)
-fitsio.write(output_dir+'metacal_output_seed'+str(SEED)+'_negative.fits', output, clobber=True)
+    #now metacalibrate the positive g1 output:
+    output_dir = '/home/secco/scratch-midway2/delve/simulated_mcal/'
+    time3 = time.time()
+    output = run_metacal([meds_dir+input_medsname],SEED)
+    time4 = time.time()
+    fitsio.write(output_dir+'metacal_output_seed'+str(SEED)+'_positive.fits', output, clobber=True)
+    print('positive metacal run took %1.2f minutes'%((time4-time3)/60.0))
 
-#will take the SLURM process ID as the random seed - be careful here as followup runs WILL have repeated SEEDS
-'''
+
+    #now replace all of the objects in this by simulated objects
+    replace_all_cutouts_by_sims(meds_dir+input_medsname,SEED, -0.02, 0.00, verbose=True)
+    #now metacalibrate the negative g1 output:
+    output_dir = '/home/secco/scratch-midway2/delve/simulated_mcal/'
+    output = run_metacal([meds_dir+input_medsname],SEED)
+    fitsio.write(output_dir+'metacal_output_seed'+str(SEED)+'_negative.fits', output, clobber=True)
+
+
+
 
